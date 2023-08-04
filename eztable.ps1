bc195e1507a7dd0817f628ffed5a2d3ed572ba1f
Nick Stambaugh <73977662+NicholasStambaugh@users.noreply.github.com>
2023-08-04 11:43:24 -0400

    Update eztable.ps1

--- eztable.ps1
+++ eztable.ps1
@@ -3,6 +3,27 @@ $html = @"
 <html>
 <head>
     <title>Data Table</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: auto;
+            margin-top: 20px;
+        }
+        th, td {
+            padding: 8px;
+            text-align: center;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        tr:hover {
+            background-color: #ddd;
+        }
+    </style>
 </head>
 <body>
     <table border="1">
@@ -14,10 +35,10 @@ $html = @"
 
 $csvData = Import-Csv -Path "data.csv"
 foreach ($entry in $csvData) {
-$html += " <tr>rn"
-$html += " <td>$($entry.Date)</td>rn"
-$html += " <td>$($entry.Value)</td>rn"
-$html += " </tr>rn"
+$html += " <tr>"
+$html += " <td>$($entry.Date)</td>"
+$html += " <td>$($entry.Value)</td>"
+$html += " </tr>"
 }
 
 $html += @"
